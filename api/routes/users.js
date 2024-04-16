7f0df20a37f0a32b6740603c4e86162e6d465091
Anirudh <anirudhvyas26@gmail.com>
2024-04-17 03:21:50 +0530

    some changes

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,7 +5,7 @@ const verify = require("../verifyToken");
 
 //UPDATE
 router.put("/:id", verify, async (req, res) => {
-    console.log(req.params.id)
+  console.log(req.params.id);
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
       req.body.password = CryptoJS.AES.encrypt(
@@ -14,11 +14,15 @@ router.put("/:id", verify, async (req, res) => {
       ).toString();
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      },{
-        new:true}
-    );
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
       res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
@@ -31,6 +35,46 @@ router.put("/:id", verify, async (req, res) => {
 module.exports = router;
 
 //DELETE
+router.delete("/:id", verify, async (req, res) => {
+  console.log(req.params.id);
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you can delete only your account! ");
+  }
+});
 //GET
+
+router.get("/find/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const user = await User.findById(req.params.id);
+
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //GETALL
+router.get("/", verify, async (req, res) => {
+  const query = request.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query ? await User.find().limit(10) : await User.find();
+
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you are not allowed to see all users");
+  }
+});
 //GET USER STATS
