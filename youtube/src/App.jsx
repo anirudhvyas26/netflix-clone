3385bcaacc28edaf53fe6b3cca6b5e7c625cb14e
Anirudh <anirudhvyas26@gmail.com>
2024-04-24 01:19:40 +0530

    added

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,12 +1,59 @@
-import "./app.scss"
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  redirect,
+} from "react-router-dom";
+import Home from "./home/home";
+import "./app.scss";
 import Login from "./components/pages/watch/login/Login";
 import Register from "./components/pages/watch/register/Register";
 import Watch from "./components/pages/watch/watch";
-import Home from "./home/home";
-
+import { Movie } from "@mui/icons-material";
 
 const App = () => {
-  return <Home/>
+  const user = true;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      loader: async () => !user ? redirect('/register') : null
+    },
+    {
+      path: "/register",
+      element: <Register />,
+      loader: async () => user ? redirect('/login') : null
+   
+    },
+     
+    {
+      path: "/login",
+      element: <Login />,
+      loader: async () => !user ? redirect('/register') : null
+      
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/watch",
+      element: <Watch />,
+    },
+    {
+      path: "/movies",
+      element: <Home type="movies" />,
+    },
+    {
+      path: "/series",
+      element: <Home type="series" />,
+    },
+  ]);
+
+  return <RouterProvider
+      router={router}
+  />
 };
 
 export default App;
--- youtube/src/home/Home.jsx
+++ youtube/src/home/Home.jsx
@@ -1,22 +1,40 @@
+import Featured from "../components/featured/featured";
+import List from "../components/list/List";
+import Navbar from "../components/navbar/navbar";
+import "./home.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists ${type ? "?type=" + type: " "} &${genre ? "&genre=" + genre: ""}`,{ 
+            headers:{
+              token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MThhNzRhOGM5OTM3MGI1M2E5MmJkYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMzg5NzE5MSwiZXhwIjoxNzE2NDg5MTkxfQ.HGDuyO_avhbXR0oZ4F5bdKOEdUmJ5jeQjL4AW9PXwD8"
+            }
 
-import Featured from "../components/featured/featured"
-import List from "../components/list/List"
-import Navbar from "../components/navbar/navbar"
-import"./home.scss"
-import React from 'react'
-
-const Home = () => {
+          }
+        );
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  },[type, genre]);
   return (
     <div className="home">
-      <Navbar/>
-     <Featured type="movie"/>
-     <List/>
-     <List/>
-     <List/>
-     <List/>
-     <List/>
+      <Navbar />
+      <Featured type={type} />
+      <List />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
