b2d8b2f6715d870cc0af7e5ca2557b7f02ee5311
Anirudh <anirudhvyas26@gmail.com>
2024-04-24 18:59:06 +0530

    check api and cors added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
+const checkRoute = require("./routes/check");
 const userRoute = require("./routes/users");
 const moviesRoute = require("./routes/movies");
 const listRoute = require("./routes/lists");
@@ -15,7 +17,9 @@ mongoose
   .then(() => console.log("DB connection is successful"))
   .catch((err) => console.log(err));
 app.use(express.json());
+app.use(cors());
 
+app.use("/api/check", checkRoute)
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", moviesRoute);
