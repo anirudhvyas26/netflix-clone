f6748f11da0624206f75d5ae659d14704f9c7cab
Anirudh <anirudhvyas26@gmail.com>
2024-04-27 04:59:02 +0530

    few errors sorted

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -56,45 +56,43 @@ router.delete("/:id", verify, async (req, res) => {
 router.get("/find/:id", verify, async (req, res) => {
   try {
     const movie = await Movies.findById(req.params.id);
-
+    console.log(req.params, movie);
     res.status(200).json(movie);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 //GET RANDOM
-router.get("/random", verify , async (req, res)=>{
-    const type = req.query.type;
-    let movie;
-    try{
-if(type === "series"){
-    movie = await Movies.aggregate([
-        { $match : { isSeries: true}},
-        { $sample:{ size: 1}},
-
-    ]);
-}else{ movie = await Movies.aggregate([
-    { $match : { isSeries: false }},
-        { $sample:{ size: 1 }},
-]);
-
-} res.status(200).json(movie);
-    
-    }catch(err){
-res.status(500).json(err);
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === "series") {
+      movie = await Movies.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movies.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
     }
+    res.status(200).json(movie);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 //GET ALL
-router.get("/", verify, async(req,res)=>{
-    if(req.user.isAdmin){
-        try{
-            const movies =await Movies.find();
-            res.status(200).json(movies.reverse());
-        }catch(err){
-            res.status(500).json(err);
-        }
-    } else{
-        res.status(403).json("You are not allowed!")
-
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movies.find();
+      res.status(200).json(movies.reverse());
+    } catch (err) {
+      res.status(500).json(err);
     }
-} );
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
